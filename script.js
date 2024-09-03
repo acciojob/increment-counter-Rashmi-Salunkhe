cafb9cc36ab92d1513f74e6261c82479845a4c91
Rashmi-Salunkhe <158038788+Rashmi-Salunkhe@users.noreply.github.com>
2024-09-03 21:48:07 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 //your JS code here. If required.
+document.addEventListener('DOMContentLoaded', function () {
+    const counterElement = document.getElementById('counter');
+    const incrementBtn = document.getElementById('incrementBtn');
+
+    let count = 0;
+
+    incrementBtn.addEventListener('click', function () {
+        alert(`Current value: ${count}`);
+        count++;
+        counterElement.textContent = count;
+    });
+});
